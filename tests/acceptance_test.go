88255aabc914e95ae9c3d652797efc4dbe62728c
Jesse Alford <jalford@pivotal.io>
2018-01-02 16:38:36 -0700

    Extract shared behavior expectations to be used with multiple contexts [#153997365]
    Signed-off-by: Milan Klanjsek <mklanjsek@pivotal.io>

--- tests/acceptance_test.go
+++ tests/acceptance_test.go
@@ -70,64 +70,75 @@ var _ = Describe("Forwarding loglines to a TCP syslog drain", func() {
 		Eventually(session).Should(gexec.Exit(0))
 	}
 
-	BeforeEach(func() {
-		Cleanup()
-		Deploy("manifest.yml")
-	})
+	TestSharedBehavior := func() {
+		Context("When messages are written to UDP with logger", func() {
+			It("receives messages in rfc5424 format on the configured drain", func() {
+				SendLogMessage("test-rfc5424")
+				Eventually(func() *gexec.Session {
+					return ForwarderLog()
+				}).Should(gbytes.Say("test-rfc5424"))
 
-	Context("When messages are written to UDP with logger", func() {
-		It("receives messages in rfc5424 format on the configured drain", func() {
-			SendLogMessage("test-rfc5424")
-			Eventually(func() *gexec.Session {
-				return ForwarderLog()
-			}).Should(gbytes.Say("test-rfc5424"))
-
-			output := LogOutput{}
-			err := json.Unmarshal(ForwarderLog().Out.Contents(), &output)
-			Expect(err).ToNot(HaveOccurred())
-
-			logs := bytes.NewBufferString(output.Tables[0].Rows[0].Stdout)
-			reader := bufio.NewReader(logs)
-
-			for {
-				line, _, err := reader.ReadLine()
+				output := LogOutput{}
+				err := json.Unmarshal(ForwarderLog().Out.Contents(), &output)
 				Expect(err).ToNot(HaveOccurred())
-				if len(line) == 0 {
-					break
+
+				logs := bytes.NewBufferString(output.Tables[0].Rows[0].Stdout)
+				reader := bufio.NewReader(logs)
+
+				for {
+					line, _, err := reader.ReadLine()
+					Expect(err).ToNot(HaveOccurred())
+					if len(line) == 0 {
+						break
+					}
+					logLine, err := syslog.Parse(line)
+					Expect(err).ToNot(HaveOccurred())
+					if string(logLine.Message()) == "test-rfc5424" {
+						sdata := logLine.StructureData()
+						Expect(string(sdata.ID())).To(Equal("instance@47450"))
+						break
+					}
 				}
-				logLine, err := syslog.Parse(line)
-				Expect(err).ToNot(HaveOccurred())
-				if string(logLine.Message()) == "test-rfc5424" {
-					sdata := logLine.StructureData()
-					Expect(string(sdata.ID())).To(Equal("instance@47450"))
-					break
-				}
-			}
+			})
+
+			It("recieves messages over 1k long on the configured drain", func() {
+				message := counterString(1025, "A")
+				SendLogMessage(message)
+				Eventually(func() *gexec.Session {
+					return ForwarderLog()
+				}).Should(gbytes.Say(message))
+			})
 		})
 
-		It("recieves messages over 1k long on the configured drain", func() {
-			message := counterString(1025, "A")
-			SendLogMessage(message)
-			Eventually(func() *gexec.Session {
-				return ForwarderLog()
-			}).Should(gbytes.Say(message))
-		})
-	})
+		Context("when a file is created in the watched directory structure", func() {
+			BeforeEach(func() {
+				session := BoshCmd("ssh", "forwarder", "-c", "sudo touch /var/vcap/sys/log/syslog_forwarder/file.log")
+				Eventually(session).Should(gexec.Exit(0))
+			})
 
-	Context("when a file is created in the watched directory structure", func() {
+			It("forwards new lines written to the file through syslog", func() {
+				Eventually(WriteToTestFile("test-blackbox-forwarding")).Should(gbytes.Say("test-blackbox-forwarding"))
+			})
+		})
+	}
+
+	Context("when file forwarding is configured to use UDP", func() {
 		BeforeEach(func() {
-			session := BoshCmd("ssh", "forwarder", "-c", "sudo touch /var/vcap/sys/log/syslog_forwarder/file.log")
-			Eventually(session).Should(gexec.Exit(0))
+			Cleanup()
+			Deploy("manifest.yml")
 		})
+		TestSharedBehavior()
+	})
 
-		It("forwards new lines written to the file through syslog", func() {
-			Eventually(WriteToTestFile("test-blackbox-forwarding")).Should(gbytes.Say("test-blackbox-forwarding"))
+	XContext("when file forwarding is configured to use TCP", func() {
+		BeforeEach(func() {
+			Cleanup()
+			Deploy("manifest.yml")
 		})
-
-		XIt("fowards messages of over 1KB", func() {
+		TestSharedBehavior()
+		It("fowards messages of over 1KB", func() {
 			message := counterString(1025, "A")
 
-			By("Wait for the new file to be detected")
 			Eventually(WriteToTestFile(message)).Should(gbytes.Say(message))
 		})
 	})
