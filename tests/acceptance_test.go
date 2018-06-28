bd0a9e063be8ad2b319e80ef2bde1e037e051fcc
Jesse Alford <jalford@pivotal.io>
2018-06-27 18:16:44 -0600

    Switch to better regex for multi-OS support
    - `stat` has inconsistent quotes across linux distros
    - Adjusting the test regex to not care about quotes
      brought it to my attention that there were other unescaped `.`s in the regex.
    - The double-quote-based "interpreted string literal" in Golang
      is a pain in the ass to escape in
      (you end up needing to escape your escape),
      so I've switched to the backtick-based "raw string literal" so it's clear
      what I am and am not escaping.
    
      [#158044717]

--- tests/acceptance_test.go
+++ tests/acceptance_test.go
@@ -29,9 +29,9 @@ var _ = Describe("Impact on the local VM", func() {
 
 		It("Cleans up any files at old config file locations", func() {
 			session := ForwarderSshCmd("stat /etc/rsyslog.d/rsyslog.conf || stat /etc/rsyslog.d/30-syslog-release.conf || stat /etc/rsyslog.d/20-syslog-release-custom-rules.conf")
-			Eventually(session).Should(gbytes.Say("stat: cannot stat '/etc/rsyslog.d/rsyslog.conf': No such file or directory"))
-			Eventually(session).Should(gbytes.Say("stat: cannot stat '/etc/rsyslog.d/30-syslog-release.conf': No such file or directory"))
-			Eventually(session).Should(gbytes.Say("stat: cannot stat '/etc/rsyslog.d/20-syslog-release-custom-rules.conf': No such file or directory"))
+			Eventually(session).Should(gbytes.Say(`stat: cannot stat ./etc/rsyslog\.d/rsyslog\.conf.: No such file or directory`))
+			Eventually(session).Should(gbytes.Say(`stat: cannot stat ./etc/rsyslog\.d/30-syslog-release\.conf.: No such file or directory`))
+			Eventually(session).Should(gbytes.Say(`stat: cannot stat ./etc/rsyslog\.d/20-syslog-release-custom-rules\.conf.: No such file or directory`))
 		})
 	})
 
